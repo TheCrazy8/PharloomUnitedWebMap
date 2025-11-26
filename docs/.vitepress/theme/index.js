ddf95fe5a8f10261bd1485c4e46647119a95fdc7
Scarlett/Eight <bennettbashford@gmail.com>
2025-11-26 18:46:28 -0500

    Add image viewer plugin to VitePress theme
    Enhanced the VitePress theme with image viewer support.

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,19 @@
-import DefaultTheme from 'vitepress/theme'
+import DefaultTheme from 'vitepress/theme';
+import 'viewerjs/dist/viewer.min.css';
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import { useRoute } from 'vitepress';
 
 export default {
-  ...DefaultTheme
+...DefaultTheme,
+enhanceApp(ctx) {
+DefaultTheme.enhanceApp(ctx);
+// Register global component (optional)
+ctx.app.component('vImageViewer', vImageViewer);
+},
+setup() {
+const route = useRoute();
+// Enable the plugin
+imageViewer(route);
 }
+};
