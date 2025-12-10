e70b7987e1cb78b61e7e9f916ea842ad52ab6461
Scarlett/Eight <bennettbashford@gmail.com>
2025-12-10 12:35:36 -0500

    Import custom CSS in theme index file

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,5 @@
 import VPLTheme from '@lando/vitepress-theme-default-plus';
+import './custom.css'
 import 'viewerjs/dist/viewer.min.css';
 import imageViewer from 'vitepress-plugin-image-viewer';
 import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
